4be82429fef9ef2aa2490bf6138527fea253db05
aoqlsdl <hyeeev@gmail.com>
2024-12-17 17:43:10 +0900

    chore: vercel build를 위한 index.py 생성

---
+++ app/index.py
@@ -0,0 +1,12 @@
+from app import create_app
+
+app = create_app()
+
+
+# 매핑 출력(디버깅용)
+with app.app_context():
+    print(app.url_map)
+
+# flask 앱 실행
+if __name__ == '__main__':
+    app.run(debug=True)
