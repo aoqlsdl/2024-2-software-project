9d1aaa84f75e995397c652dd07af635e5c45e461
매빈 <77741296+aoqlsdl@users.noreply.github.com>
2024-12-17 19:46:24 +0900

    fix: cors 설정

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,9 @@
 from flask import Flask
+from flask_cors import CORS
 
 def create_app():
     app = Flask(__name__)
+    CORS(app)
 
     # Blueprint 등록
     from .match.match_controller import match_bp
